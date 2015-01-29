2f4e8d91dcd7621a6c0ea8a09137188e5b5b62e9
Vitaliy Tarash <tarash@steelkiwi.com>
2015-01-29 15:20:24 +0200

    init

--- angular-localize.js
+++ angular-localize.js
@@ -43,21 +43,22 @@
         }])
 
         .factory('localize', [
-            '$filter', 'localizeConfig',
-            function ($filter, localizeConfig) {
-                var i18n = localizeConfig.i18n,
-                    escapeHTML = $filter('escapeHTML');
+            '$filter', 'localizeConfig', '$window', 'language',
+            function ($filter, localizeConfig, $window, language) {
+                var escapeHTML = $filter('escapeHTML');
                 return function (key, data, escape) {
-                    var func = i18n[key],
-                        escapedData;
-                    if (func) {
-                        if (escape) {
-                            escapedData = {};
-                            angular.forEach(data, function (value, key) {
-                                escapedData[key] = escapeHTML(value);
-                            });
+                    if($window.i18n){
+                        var func = ($window.i18n)[key],
+                            escapedData;
+                        if (func) {
+                            if (escape) {
+                                escapedData = {};
+                                angular.forEach(data, function (value, key) {
+                                    escapedData[key] = escapeHTML(value);
+                                });
+                            }
+                            return func(escapedData || data || {});
                         }
-                        return func(escapedData || data || {});
                     }
                     return key;
                 };
@@ -69,70 +70,84 @@
         }])
 
         .directive('localize', [
-            '$sanitize', '$filter', 'localizeConfig',
-            function ($sanitize, $filter, localizeConfig) {
-                var i18n = localizeConfig.i18n,
-                    escapeHTML = $filter('escapeHTML');
-                return function (scope, elm, attrs) {
-                    // Take the translation key from the element content
-                    // if the localize attribute is empty:
-                    var key = attrs.localize || elm.html(),
-                        func = i18n[key],
-                        isInput = /input|textarea/i.test(elm.prop('nodeName')),
-                        data,
-                        update,
-                        hasObservers;
-                    if (func) {
-                        if (isInput) {
-                            update = function () {
-                                attrs.$set('placeholder', func(attrs));
-                            };
-                        } else if (attrs.localize) {
-                            // Localization is text only
-                            update = function () {
-                                elm.text(func(attrs));
-                            };
-                        } else {
-                            // Localization can contain HTML
-                            data = {};
-                            update = function (key, value) {
-                                if (key) {
-                                    data[key] = escapeHTML(value);
-                                }
-                                elm.html($sanitize(func(data)));
-                            };
-                        }
-                        angular.forEach(attrs.$attr, function (attr, normAttr) {
-                            if (localizeConfig.observableAttrs.test(attr)) {
-                                attrs.$observe(
-                                    normAttr,
-                                    isInput || attrs.localize ? update :
-                                        function (value) {
-                                            update(normAttr, value);
+            '$compile', '$sanitize', '$filter', 'localizeConfig', '$window',
+            function ($compile, $sanitize, $filter, localizeConfig, $window) {
+                return {
+                    scope: true,
+                    link: function (scope, elm, attrs) {
+                        var escapeHTML = $filter('escapeHTML');
+                        // Take the translation key from the element content
+                        // if the localize attribute is empty:
+                        // var unlisten = scope.$on('localeChanged', function(){
+                        //  $compile(elm)(scope);
+                        //  unlisten();
+                        // })
+                        var key = attrs.localize || elm.html(),
+                            // func = $window.i18n[key],
+                            isInput = /input|textarea/i.test(elm.prop('nodeName')),
+                            data,
+                            update,
+                            hasObservers;
+                        scope.window = $window;
+                        scope.key = key;
+                        scope.$watch('window.i18n[key]', function(func){
+                            if (func) { 
+                                if (isInput) {
+                                    update = function () {
+                                        attrs.$set('placeholder', func(attrs));
+                                    };
+                                } else if (attrs.localize) {
+                                    // Localization is text only
+                                    update = function () {
+                                        elm.text(func(attrs));
+                                    };
+                                } else {
+                                    // Localization can contain HTML
+                                    data = {};
+                                    update = function (key, value) {
+                                        if (key) {
+                                            data[key] = value;//escapeHTML(value);
                                         }
-                                );
-                                hasObservers = true;
+                                        // elm.html($sanitize(func(data)));
+                                        elm.html(func(data));
+                                        if(attrs.$attr.compile)
+                                            $compile(elm.contents())(scope);
+                                    };
+                                }
+                                angular.forEach(attrs.$attr, function (attr, normAttr) {
+                                    if (localizeConfig.observableAttrs.test(attr)) {
+                                        attrs.$observe(
+                                            normAttr,
+                                            isInput || attrs.localize ? update :
+                                                function (value) {
+                                                    update(normAttr, value);
+                                                }
+                                        );
+                                        hasObservers = true;
+                                    }
+                                });
+                                // if (!hasObservers) {
+                                //  update();
+                                // }
+                                update();
+                            } else if (attrs.localize) {
+                                // If there is no translation function,
+                                // the key itself is the translation value:
+                                if (isInput) {
+                                    attrs.$set('placeholder', key);
+                                } else {
+                                    elm.text(key);
+                                }
                             }
                         });
-                        if (!hasObservers) {
-                            update();
-                        }
-                    } else if (attrs.localize) {
-                        // If there is no translation function,
-                        // the key itself is the translation value:
-                        if (isInput) {
-                            attrs.$set('placeholder', key);
-                        } else {
-                            elm.text(key);
-                        }
                     }
                 };
             }
         ])
 
         .factory('localizeFactory', [
-            'localizeConfig',
-            function (localizeConfig) {
+            'localizeConfig', '$window',
+            function (localizeConfig, $window) {
                 var i18n = localizeConfig.i18n;
                 return function () {
                     var directiveObj = {
@@ -140,7 +155,7 @@
                             var name = directiveObj.name,
                                 target = name.charAt(8).toLowerCase() + name.slice(9),
                                 key = attrs[name],
-                                func = i18n[key],
+                                func = $window.i18n[key],
                                 update,
                                 hasObservers;
                             if (func) {
